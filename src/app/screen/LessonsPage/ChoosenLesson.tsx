130758911d9278ab4a91777346ea29ad64c3d402
DixonKH <khaydarovdilshod9@gmail.com>
2024-12-06 00:23:20 +0900

    feat:changed  detailLesson

--- src/app/screen/LessonsPage/ChoosenLesson.tsx
+++ src/app/screen/LessonsPage/ChoosenLesson.tsx
@@ -1,14 +1,19 @@
 import { IoMdEye } from "react-icons/io";
 import { CiHeart } from "react-icons/ci";
 import { FaStar } from "react-icons/fa6";
+import { MdMarkEmailUnread } from "react-icons/md";
+import { FaBookReader } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
 import { BsPatchExclamationFill } from "react-icons/bs";
+import { BsTelephoneFill } from "react-icons/bs";
 import video from "/videos/lesson.mp4";
 import LessonReview from "./LessonReview";
+import Person from "/public/images/person1.jpg";
 
 export default function ChoosenLesson() {
   return (
     <div className="container mt-5">
-      <div className="flex items-center justify-between">
+      <div className="flex items-start justify-between gap-4">
         <div className="w-3/4 flex flex-col items-start justify-start">
           <div className="w-full flex flex-row items-center justify-between">
             <div className="text-2xl">
@@ -44,7 +49,7 @@ export default function ChoosenLesson() {
               amet consectetur, adipisicing elit. Repudiandae temporibus ducimus
               assumenda culpa voluptatem a quisquam ipsum asperiores eos nihil?
             </p>
-            <div className="flex flex-row items-center justify-between gap-2">
+            <div className="flex flex-row items-center justify-between gap-2 mt-5">
               <p className="flex flex-row items-center justify-start gap-1">
                 <FaStar className="text-yellow text-lg" />
                 <FaStar className="text-yellow" />
@@ -56,13 +61,71 @@ export default function ChoosenLesson() {
               </p>
               <p className="flex items-center justify-start gap-1">
                 <BsPatchExclamationFill className="text-yellow" />
-                <p>Last updated: <span className="ml-4 italic text-slate-500">2024-01-01</span></p>
+                <p>
+                  Last updated:{" "}
+                  <span className="ml-4 italic text-slate-500">2024-01-01</span>
+                </p>
               </p>
             </div>
           </div>
           <LessonReview />
         </div>
-        <div className="">2</div>
+        <div className="flex flex-col justify-start border mt-2 rounded-md p-4 gap-1">
+          <div className="flex flex-col items-center justify-start gap-1">
+            <h1 className="text-2xl font-bold">Jahongir B.</h1>
+            <p className="text-sm text-slate-500">English Teacher</p>
+            <div className="w-32 h-32 my-2 rounded-full overflow-hidden">
+              <img
+                className="object-cover w-full h-full"
+                src={Person}
+                alt="Person"
+              />
+            </div>
+            <p className="text-lg text-slate-800">About me</p>
+            <p className="w-64 text-sm text-slate-500 text-center">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a
+              consectetur felis. Sed id sagittis neque, vel facilisis nunc.
+              Donec consequat, enim vel tristique fermentum, arcu lectus semper
+              velit, vel facilisis arcu eros vel velit.
+            </p>
+          </div>
+          <div className="flex flex-col items-start justify-start gap-2 text-sm">
+            <p className="flex flex-row items-center justify-start gap-1 text-slate-600">
+              <FaBookReader className="text-green" />
+              Experience: 10 years
+            </p>
+            <p className="flex flex-row items-center justify-start gap-1 text-slate-600">
+              <FaLocationDot className="text-green" />
+              Location: Tashkent
+            </p>
+            <p className="flex flex-row items-center justify-start gap-1 text-slate-600">
+              <MdMarkEmailUnread className="text-green" />
+              Mail: example@gmail.com
+            </p>
+            <p className="flex flex-row items-center justify-start gap-1 text-slate-600">
+              <BsTelephoneFill className="text-green" />
+              Phone: +998991234567
+            </p>
+            <div className="w-full flex flex-row items-center justify-center gap-8 mt-3">
+              <p>
+                <a
+                  href="https://t.me/example"
+                  className="text-white bg-green p-1 px-2 rounded-lg"
+                >
+                  Telegram
+                </a>
+              </p>
+              <p>
+                <a
+                  href="https://www.instagram.com/example"
+                  className="text-white bg-green p-1 px-2 rounded-lg"
+                >
+                  Instagram
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
       <div>Realited Videos</div>
       <div>Videoooooos</div>
--- src/app/screen/LessonsPage/LessonReview.tsx
+++ src/app/screen/LessonsPage/LessonReview.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export default function LessonReview() {
   return (
-    <div>
-      Hello
+    <div className="w-full flex flex-col mt-6">
+      <p className="text-2xl font-bold">Leave a Review</p>
+      <div className="w-full flex flex-col items-end justify-center gap-2 my-2">
+        <textarea className="w-full h-32 border border-gray-300 rounded-md outline-none p-2"></textarea>
+        <button className="bg-green1 text-white px-4 py-2 rounded-md">
+          Submit
+        </button>
+      </div>
     </div>
-  )
+  );
 }
