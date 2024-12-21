4e8c1ff74fdd089c3d9887e04b5031db321d294a
DixonKH <khaydarovdilshod9@gmail.com>
2024-12-21 19:11:38 +0900

    fix: fixed TeachersPage

--- src/app/screen/OurTeachers/TeacherHeader.tsx
+++ src/app/screen/OurTeachers/TeacherHeader.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Line from '../MainPage/Line'
 import { FaBook } from "react-icons/fa6";
+import { useTranslation } from 'react-i18next';
 
 export default function TeacherHeader() {
+  const { t } = useTranslation();
   return (
     <>
       <div className="flex flex-col items-center justify-between mt-28 pt-4 w-full h-auto container gap-6">
@@ -13,19 +15,15 @@ export default function TeacherHeader() {
               <FaBook className="md:text-xl lg:text-xl sm:text-xl text-base text-white" />
             </div>
             <p className="lg:text-2xl md:text-xl text-center text-xl">
-              Korean Language Lessons 
+              {t("teacher")}
             </p>
           </div>
           <h1 className="lg:text-5xl text-2xl text-center font-bold text-green2 mb-4 lg:mt-0 mt-8">
-            Korean Language Lessons{" "}<br/>
-            <span className="text-gray-600">from Beginners</span>{" "}
-            <span className="text-gray-600">to Advanced Learners</span>
+            {t("teacher_h1")}{" "}<br/>
+            <span className="text-gray-600">{t("teacher_h2")}</span>    
           </h1>
           <p className="lg:text-xl sm:text-md text-gray-600 text-center mb-8 w-full">
-            Discover the beauty of Korean language with our lessons and courses.
-            We will provide you with a comprehensive understanding of Korean
-            culture and grammar, allowing you to communicate effectively in this
-            fascinating language.
+            {t("teacher_p")}
           </p>
       </div>
       <Line />
