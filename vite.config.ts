be1c16009e11baf9506fbd9197bb4231ef95b052
DixonKH <khaydarovdilshod9@gmail.com>
2025-05-09 18:12:08 +0900

    feat: modified gitlab-ci.yml file

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 import Pages from "vite-plugin-pages";
 
 export default defineConfig({
-  base: "/edusmart/",
   plugins: [react(), Pages()],
   resolve: {
     alias: {
